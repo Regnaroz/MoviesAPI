a7b23954432f029a0ba9d96c9f5d22158f4bc318
Moayyad Alajlouni <moaead-alajlouni@hotmail.com>
2021-11-15 00:36:27 +0200

    ss

--- Movies.API/Controllers/CustomerController.cs
+++ Movies.API/Controllers/CustomerController.cs
@@ -90,7 +90,7 @@ namespace Movies.API.Controllers
                 //decoder for image name , no duplicate errors
                 string attachmentFileName = $"{fileName}.{Path.GetExtension(file.FileName).Replace(".", "")}";
                 //path for angualr project file
-                var fullPath = Path.Combine("C:\\Users\\Moayyad Alajlouni\\Desktop\\Angularmovies\\src\\assets\\images\\Uploaded File", attachmentFileName);
+                var fullPath = Path.Combine("C:\\Users\\Moayyad Alajlouni\\Desktop\\Angularmovies\\src\\assets\\images\\movies-images", attachmentFileName);
                 using (var stream = new FileStream(fullPath, FileMode.Create))
                 {
                     file.CopyTo(stream);
